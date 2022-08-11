9782e115be3770e6e78c8d0b6d336f885c6493a3
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-08-11 16:18:56 +0900

    Update 15688.cpp

--- 15688.cpp
+++ 15688.cpp
@@ -1,3 +1,22 @@
+//add define tie & sync
+#include<iostream>
+#include<algorithm>
+#define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+int ar[1000001];
+using namespace std;
+int main() {
+    fast;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> ar[i];
+    sort(ar, ar + n);
+    for (int i = 0; i < n; i++)
+        cout << ar[i] << '\n';
+    return 0;
+}
+
+/*
 #include<iostream>
 #include<algorithm>
 int ar[1000001];
@@ -13,4 +32,4 @@ int main() {
     for (int i = 0; i < n; i++)
         cout << ar[i] << '\n';
     return 0;
-}
+}*/
