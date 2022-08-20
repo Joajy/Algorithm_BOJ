aa146789c60df24fc4120e7bb4e5bbd7e4c42e86
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-08-20 10:25:46 +0900

    Add files via upload
    using Sort Algorithm

---
+++ 1181.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std;
+
+string ans[20001];
+
+bool cmp(string a, string b) {
+	if (a.length() == b.length()) {
+		return a < b;
+	}
+	else
+		return a.length() < b.length();
+}
+
+int main() {
+	fast;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> ans[i];
+		for (int j = 0; j < i; j++) {
+			if (ans[i] == ans[j]) {
+				i--; n--;
+				break;
+			}
+		}
+	}
+	sort(ans, ans + n, cmp);
+	for (int i = 0; i < n; i++)
+		cout << ans[i] << '\n';
+	return 0;
+}
