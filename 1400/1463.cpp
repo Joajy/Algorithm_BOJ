332a95c3274e1c637bd5ab569c51d6329b9fc461
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-08-26 13:37:14 +0900

    Update 1463.cpp
    using Dynamic Programming

--- 1400/1463.cpp
+++ 1400/1463.cpp
@@ -1,3 +1,37 @@
+//not using queue STL answer
+#include<iostream>
+#define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std;
+
+int dp[1000001];
+
+int main() {
+	fast;
+	int n, ans = 0;
+	cin >> n;
+	for (int i = n; i >= 1; i--) {
+		if (i % 3 == 0) {
+			if (dp[i / 3] == 0)
+				dp[i / 3] = dp[i] + 1;
+			else
+				dp[i / 3] = min(dp[i / 3], dp[i] + 1);
+		}
+		if (i % 2 == 0) {
+			if (dp[i / 2] == 0)
+				dp[i / 2] = dp[i] + 1;
+			else
+				dp[i / 2] = min(dp[i / 2], dp[i] + 1);
+		}
+		if (dp[i - 1] == 0)
+			dp[i - 1] = dp[i] + 1;
+		else
+			dp[i - 1] = min(dp[i - 1], dp[i] + 1);
+	}
+	cout << dp[1];
+	return 0;
+}
+
+//first answer
 #include<iostream>
 #include<queue>
 #define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
