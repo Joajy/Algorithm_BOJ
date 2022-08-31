2d4524cd384b349d7fb6b93dc60a42f4fa4c28f9
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-08-31 14:41:58 +0900

    Create 11286.cpp
    using Priority_Queue

---
+++ 11000/11286.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<queue>
+#include<utility>
+#include<cmath>
+#define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+using namespace std;
+using pi = pair<int, int>;
+int main() {
+    fast;
+	priority_queue<pi, vector<pi>> q;
+	int n, x;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (x != 0)
+			q.push({ -abs(x), -x });
+		else {
+			if (q.empty()) {
+				cout << 0 << '\n';
+				continue;
+			}
+			cout << -q.top().second << '\n';
+			q.pop();
+		}
+	}
+	return 0;
+}
