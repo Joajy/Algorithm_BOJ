da1d08f4fcb75e83053b22e7ee5cebb9b63696fe
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2022-11-12 00:55:48 +0900

    Create 5639.cpp
    using BST(Binary Search Tree)

---
+++ 5000/5639.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left, * right;
+};
+
+void preorder(Node* node) {
+    if(node->left != nullptr) preorder(node->left);
+    if (node->right != nullptr) preorder(node->right);
+    cout << node->data << '\n';
+}
+
+Node* Insert(Node* node, int data) {
+    if (node == nullptr) {
+        node = new Node;
+        node->data = data;
+        node->right = nullptr;
+        node->left = nullptr;
+    }
+    else if (data > node->data)
+        node->right = Insert(node->right, data);
+    else
+        node->left = Insert(node->left, data);
+    return node;
+}
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int num;
+    Node* root = nullptr;
+    while (cin >> num) {
+        if (num == EOF) break;
+        root = Insert(root, num);
+    }
+    preorder(root);
+    return 0;
+}
