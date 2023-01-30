dd19d9670143bbdb8e7c9629cb9d0847d624fe97
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2023-01-30 13:28:49 +0900

    가장 긴 증가하는 부분 수열 2
    LIS with Binary Search

---
+++ 12000/12015.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	int n, a;
+	vector<int> v{ -1 };
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+ 		if (a > v.back())
+			v.push_back(a);
+		else
+			*lower_bound(v.begin(), v.end(), a) = a;
+	}
+	cout << v.size() - 1;
+	return 0;
+}
