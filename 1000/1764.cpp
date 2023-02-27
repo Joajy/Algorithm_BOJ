a5289be5baacc58a3e1869fd4542bd05a42d51a7
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2023-02-27 10:57:03 +0900

    듣보잡
    1. Set & Vector container
    just general sorting
    2. Priority_Queue

--- 1000/1764.cpp
+++ 1000/1764.cpp
@@ -1,3 +1,5 @@
+//1번째 풀이방법 7580KB, 40ms
+
 #include<iostream>
 #include<set>
 #include<vector>
@@ -28,4 +30,41 @@ int main() {
     for (auto i : v)
         cout << i << '\n';
     return 0;
+}
+
+//2번째 풀이방법  9864KB, 60ms
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main() {
+	cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0);
+	int n, m;
+	string name;
+	cin >> n >> m;
+	priority_queue<string> a, b;
+	vector<string> v;
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		a.push(name);
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> name;
+		b.push(name);
+	}
+	while (!(a.empty() || b.empty())) {
+		if (a.top() == b.top()) {
+			v.push_back(a.top());
+			a.pop(); b.pop();
+		}
+		else if (a.top() > b.top()) a.pop();
+		else b.pop();
+	}
+	reverse(v.begin(), v.end());
+	cout << v.size() << '\n';
+	for (auto i : v) cout << i << '\n';
+	return 0;
 }
