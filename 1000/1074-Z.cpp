485e5b5bbac6b8da8dd5c987266e380a7f18e053
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2023-06-19 01:13:02 +0900

    Z
    Recursive DFS

--- 1000/1074-Z.cpp
+++ 1000/1074-Z.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int n, r, c, ans;
+int N, r, c, answer = 0;
 
-void Z(int y, int x, int size)
-{
-    if (y == r && x == c) {
-        cout << ans;
-        return;
-    }
-    // if r, c is in current of four Quadrant
-    if (r < y + size && r >= y && c < x + size && c >= x) {
-        // First Quadrant
-        Z(y, x, size / 2);
-        // Second Quadrant
-        Z(y, x + size / 2, size / 2);
-        // Third Quadrant
-        Z(y + size / 2, x, size / 2);
-        // Fourth Quadrant
-        Z(y + size / 2, x + size / 2, size / 2);
-    }
-    else
-        ans += size * size;
+void input() {
+	cin >> N >> r >> c;
 }
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> r >> c;
 
-    // bit extension idea
-    Z(0, 0, 1 << n);
+void dfs(int x, int y, int n) {
+	if (x == r && y == c) {
+		cout << answer;
+		return;
+	}
+	if (r >= x && r < x + n && c >= y && c < y + n) {
+		n >>= 1;
+		dfs(x, y, n);
+		dfs(x, y + n, n);
+		dfs(x + n, y, n);
+		dfs(x + n, y + n, n);
+	}
+	else {
+		answer += n * n;
+	}
+}
 
-    return 0;
+void solution() {
+	input();
+	dfs(0, 0, 1 << N);
+}
+
+int main() {
+	cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0);
+	solution();
+	return 0;
 }
