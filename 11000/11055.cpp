7a3d1443a29c3c8a2caa36cb6537533a5d02d306
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2023-07-11 23:36:03 +0900

    가장 큰 증가하는 부분 수열
    Dynamic Programming

---
+++ 11000/11055.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int n;
+int a[1001], dp[1001];
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+		dp[i] = a[i];
+	}
+}
+
+void solution() {
+	input();
+    int answer = dp[0];
+	for (int i = 1; i < n; ++i) {
+		for (int j = 0; j < i; ++j) {
+			if (a[j] < a[i] ) {
+				dp[i] = max(dp[i], dp[j] + a[i]);
+			}
+		}
+		if (dp[i] > answer) answer = dp[i];
+	}
+	cout << answer;
+}
+
+int main() {
+	cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(0);
+	solution();
+	return 0;
+}
