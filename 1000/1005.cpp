c70e947e03f25253fe02d0d07d728407c03b00f8
Mr.Kim <86274253+Joajy@users.noreply.github.com>
2023-08-24 01:46:11 +0900

    ACM Craft
    Topological Sorting

--- 1000/1005.cpp
+++ 1000/1005.cpp
@@ -1,49 +1,55 @@
 #include<iostream>
-#include<vector>
 #include<queue>
 using namespace std;
 
+int t, n, m, w;
+int indeg[1001], cost[1001], dist[1001];
 vector<int> v[1001];
-int deg[1001], period[1001], result[1001];
-int t, n, k, x, y, w, answer;
 
-void init() {
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> period[i];
-		deg[i] = 0;
-		result[i] = period[i];
+void input() {
+	cin >> n >> m;
+	int a, b;
+	for (int i = 1; i <= n; ++i) {
+		cin >> cost[i];
+		indeg[i] = 0;
+		dist[i] = cost[i];
 		v[i].clear();
 	}
-	for (int i = 0; i < k; i++) {
-		cin >> x >> y;
-		v[x].push_back(y);
-		deg[y]++;
+	for (int i = 0; i < m; ++i) {
+		cin >> a >> b;
+		v[a].push_back(b);
+		++indeg[b];
 	}
 	cin >> w;
+}
+
+void solution() {
+	input();
+	queue<int> q;
+	for (int i = 1; i <= n; ++i) {
+		if (indeg[i] == 0) {
+			q.push(i);
+		}
+	}
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+		for (int nxt : v[cur]) {
+			if (--indeg[nxt] == 0) {
+				q.push(nxt);
+			}
+			dist[nxt] = max(dist[nxt], dist[cur] + cost[nxt]);
+		}
+	}
+	cout << dist[w] << '\n';
 }
 
 int main() {
 	cin.tie(0), cout.tie(0);
 	ios_base::sync_with_stdio(0);
+	int t;
 	cin >> t;
-	while(t--) {
-		init();
-		queue<int> q;
-		for (int i = 1; i <= n; i++) {
-			if (deg[i] == 0)
-				q.push(i);
-			result[i] = period[i];
-		}
-		while (!q.empty()) {
-			int cur = q.front(); q.pop();
-			for (auto nxt : v[cur]) {
-				if (--deg[nxt] == 0)
-					q.push(nxt);
-				result[nxt] = max(result[nxt], result[cur] + period[nxt]);
-			}
-		}
-		cout << result[w] << '\n';
+	for (int i = 0; i < t; ++i) {
+		solution();
 	}
 	return 0;
 }
