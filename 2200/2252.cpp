83225583a84b41c1630b1ba7a8d6faafcd5590c7
Mr.Kim <joajy98@gmail.com>
2023-10-13 00:24:50 +0900

    줄 세우기
    Topological Sort

--- 2200/2252.cpp
+++ 2200/2252.cpp
@@ -2,38 +2,40 @@
 #include<vector>
 #include<queue>
 using namespace std;
+int n, m;
+int indeg[32001];
+vector<int> v[32001];
 
-vector<int> v[32002];
-int indeg[32002];
-
-int main() {
-	cin.tie(0); cout.tie(0);
-  ios_base::sync_with_stdio(0);
-	int n, m;
+void input() {
 	cin >> n >> m;
-	vector<int> ans;
-	queue<int> q;
-	for (int i = 0; i < m; i++) {
-		int a, b;
+	int a, b;
+	for (int i = 0; i < m; ++i) {
 		cin >> a >> b;
 		v[a].push_back(b);
-		indeg[b]++;
+		++indeg[b];
 	}
-	for (int i = 1; i <= n; i++)
-		if (indeg[i] == 0)
-			q.push(i);
+}
+
+void solution() {
+	input();
+	queue<int> q;
+	for (int i = 1; i <= n; ++i) {
+		if (indeg[i] == 0) q.push(i);
+	}
 	while (!q.empty()) {
 		int cur = q.front(); q.pop();
-		ans.push_back(cur);
+		cout << cur << ' ';
 		for (int nxt : v[cur]) {
-			indeg[nxt]--;
-			if (indeg[nxt] == 0)
+			if (--indeg[nxt] == 0) {
 				q.push(nxt);
+			}
 		}
 	}
-	if (ans.size() != n) return 0;
-	for (auto i : ans)
-		cout << i << ' ';
+}
 
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	solution();
 	return 0;
 }
