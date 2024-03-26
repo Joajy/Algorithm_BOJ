9e4036588b51c252d4bf3b7a512de0d90cd9fcb8
Joajy <joajy98@gmail.com>
2024-03-26 20:33:45 +0900

    가장 긴 증가하는 부분 수열 3
    Longest Increase Sequence

---
+++ 10000/12783.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static int[] a;
+
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void solution() {
+        Vector<Integer> list = new Vector<>();
+        list.add((int)-2e9);
+        for (int i = 0; i < n; ++i) {
+            int last = list.lastElement();
+            if(a[i] > last){
+                list.add(a[i]);
+            }
+            else{
+                int idx = Collections.binarySearch(list, a[i]);
+                if(idx < 0) idx = -idx - 1;
+                list.set(idx, a[i]);
+            }
+        }
+        System.out.println(list.size() - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
