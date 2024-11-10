737c3e77616d12bb242a2b7bcdb793f0e73148fd
Joajy <joajy98@gmail.com>
2024-11-10 23:43:55 +0900

    [11724] 연결 요소의 개수
    BFS, DFS, Stack, Union-Find

--- 11000/11724.cpp
+++ 11000/11724.cpp
@@ -36,3 +36,49 @@ int main() {
     cout << ans;
     return 0;
 }
+
+
+//2. Union-Find
+// #include<iostream>
+// #define fastio cin.tie(0), ios_base::sync_with_stdio(0);
+
+// using namespace std;
+
+// int n, m;
+// int par[1001];
+
+// int find(int cur) {
+// 	if (par[cur] == cur) return cur;
+// 	return par[cur] = find(par[cur]);
+// }
+
+// void input() {
+// 	cin >> n >> m;
+// 	for (int i = 1; i <= n; ++i) {
+// 		par[i] = i;
+// 	}
+// 	int a, b;
+// 	for (int i = 0; i < m; ++i) {
+// 		cin >> a >> b;
+// 		a = find(a);
+// 		b = find(b);
+// 		if (a == b) continue;
+// 		par[a] = b;
+// 	}
+// }
+
+// void solution() {
+// 	int answer = 0;
+// 	for (int i = 1; i <= n; ++i) {
+// 		if (par[i] == i) ++answer;
+// 	}
+// 	cout << answer << '\n';
+// }
+
+
+// int main() {
+// 	fastio;
+// 	input();
+// 	solution();
+// 	return 0;
+// }
